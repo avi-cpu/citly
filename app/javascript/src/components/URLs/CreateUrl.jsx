b9a9db83b1aece58839b56708646eba4b5d77f7a
avi-cpu <82474599+avi-cpu@users.noreply.github.com>
2021-07-17 09:50:55 +0530

    Update CreateUrl.jsx

--- app/javascript/src/components/URLs/CreateUrl.jsx
+++ app/javascript/src/components/URLs/CreateUrl.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import UrlForm from "./Form/UrlForm.jsx";
-//import urlsApi from "apis/urls";
 
 const CreateUrl = ({ url, setUrl, loading, handleSubmit }) => {
   return (
