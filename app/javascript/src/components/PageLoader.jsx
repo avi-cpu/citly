76abdd0a302503b180bb7a5505f1fa6488bc51d8
avi-cpu <82474599+avi-cpu@users.noreply.github.com>
2021-07-17 09:50:31 +0530

    Update PageLoader.jsx

--- app/javascript/src/components/PageLoader.jsx
+++ app/javascript/src/components/PageLoader.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const PageLoader = () => {
   return (
     <div className="flex flex-row items-center justify-center w-screen h-screen">
-      {/* <h1 className="text-lg leading-5">Loading...</h1> */}
       <img src={window.loader} />
     </div>
   );
